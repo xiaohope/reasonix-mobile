b31e68bd73c6edf03b2b79639610eb9374355a48
user <user@github.com>
2026-06-04 11:03:08 +0800

    Fix buildscript repos and bump file_picker

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,9 +1,3 @@
-buildscript {
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.11.1")
-    }
-}
-
 allprojects {
     repositories {
         google()
@@ -22,9 +16,8 @@ subprojects {
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 
     afterEvaluate {
-        // Force compileSdk to 36 for all Android subprojects (fixes file_picker issue)
-        if (plugins.hasPlugin("com.android.application") || plugins.hasPlugin("com.android.library")) {
-            extensions.findByType(com.android.build.gradle.BaseExtension::class.java)?.compileSdkVersion(36)
+        if (extensions.findByName("android") != null) {
+            setProperty("android.compileSdk", 36)
         }
     }
 }
